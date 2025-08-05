007f5752743c7c6db7ebc3f40045d8e5a25c0ca4
Mubarek Shikur <manmuba90@gmail.com>
2025-08-05 21:39:35 +0300

    fix: Add static file serving to urlpatterns and update docker-compose command

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,3 +36,4 @@ urlpatterns = [
     path('api/feedback/', include('feedback.urls')),
     path('api/', include('core.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
